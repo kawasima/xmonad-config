a4592a99758c7b79b5e1b2fa6b728d4dd74bdb3b
Rod Vagg <rod@vagg.org>
2014-01-21 22:58:18 +1100

    fixes and improvements

--- xmonad.hs
+++ xmonad.hs
@@ -48,7 +48,11 @@ myKeys =
     , ((myModMask .|. shiftMask  , xK_q)     , spawn ("gnome-session-quit"))
       -- lock screen
     , ((mod1Mask  .|. shiftMask  , xK_l)     , spawn ("dm-tool lock"))
-    , ((mod1Mask                 , xK_F4)     , kill)
+    , ((mod1Mask                 , xK_F4)    , kill)
+      -- shrink current window
+    , ((myModMask                , xK_a)     , sendMessage MirrorShrink)
+      -- expand current window
+    , ((myModMask                , xK_z)     , sendMessage MirrorExpand)
   ]
 
 {-
@@ -73,6 +77,57 @@ myManagementHooks :: [ManageHook]
 myManagementHooks = [
   ]
 
+{-
+  Layout configuration. In this section we identify which xmonad
+  layouts we want to use. I have defined a list of default
+  layouts which are applied on every workspace, as well as
+  special layouts which get applied to specific workspaces.
+
+  Note that all layouts are wrapped within "avoidStruts". What this does
+  is make the layouts avoid the status bar area at the top of the screen.
+  Without this, they would overlap the bar. You can toggle this behavior
+  by hitting "super-b" (bound to ToggleStruts in the keyboard bindings
+  in the next section).
+-}
+
+-- Define group of default layouts used on most screens, in the
+-- order they will appear.
+-- "smartBorders" modifier makes it so the borders on windows only
+-- appear if there is more than one visible window.
+-- "avoidStruts" modifier makes it so that the layout provides
+-- space for the status bar at the top of the screen.
+defaultLayouts = smartBorders(avoidStruts(
+  -- ResizableTall layout has a large master window on the left,
+  -- and remaining windows tile on the right. By default each area
+  -- takes up half the screen, but you can resize using "super-h" and
+  -- "super-l".
+  ResizableTall 1 (3/100) (1/2) []
+
+  -- Mirrored variation of ResizableTall. In this layout, the large
+  -- master window is at the top, and remaining windows tile at the
+  -- bottom of the screen. Can be resized as described above.
+  ||| Mirror (ResizableTall 1 (3/100) (1/2) [])
+
+  -- Full layout makes every window full screen. When you toggle the
+  -- active window, it will bring the active window to the front.
+  ||| noBorders Full
+
+  -- Grid layout tries to equally distribute windows in the available
+  -- space, increasing the number of columns and rows as necessary.
+  -- Master window is at top left.
+  ||| Grid
+
+  -- ThreeColMid layout puts the large master window in the center
+  -- of the screen. As configured below, by default it takes of 3/4 of
+  -- the available space. Remaining windows tile to both the left and
+  -- right of the master window. You can resize using "super-h" and
+  -- "super-l".
+  ||| ThreeColMid 1 (3/100) (3/4)
+
+  -- Circle layout places the master window in the center of the screen.
+  -- Remaining windows appear in a circle around it
+  ||| Circle))
+
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
   xmonad $ gnomeConfig {
@@ -82,6 +137,7 @@ main = do
   , focusedBorderColor  = myFocusedBorderColor
   , borderWidth         = myBorderWidth
   , handleEventHook     = fullscreenEventHook
+  , layoutHook          = defaultLayouts
   , startupHook         = do
       setWMName "LG3D"
       spawn "~/.xmonad/startup-hook"
